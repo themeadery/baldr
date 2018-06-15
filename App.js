3e7bd2c2c59c2522f6e55aaab9df0aee80657588
Matt Mead <matt.a.mead@gmail.com>
2018-06-14 23:27:28 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Picker,
+  Button
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 10.0};
+  }
+
+  //This is going to be where it calculates the Brix to SG
+  //and saves the value into the state/var
+  onClick = () => {
+    this.setState((prevState) => ({
+      value: prevState.value + 10
+    }));
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Brix to SG
+        </Text>
+
+        {/*Change between unfermented wort/must and fermenting*/}
+        <Picker
+          selectedValue={this.state.wort}
+          style={styles.picker}
+          onValueChange={(itemValue, itemIndex) => this.setState({ wort: itemValue })}>
+          <Picker.Item label="Unfermented" value="unfermented" />
+          <Picker.Item label="Fermenting" value="fermenting" />
+        </Picker>
+		
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          autoFocus={true}
+          placeholder="10.0"
+          keyboardType="numeric"
+          maxLength={5}
+          onChangeText={(value) => this.setState({ value })}
+        />
+
+        {/*Show the calculated value*/}
+        <Text style={styles.calculated}>
+          Specific Gravity: {this.state.value}
+        </Text>
+
+        {/*The only reason this is here is to call the onClick function to do some math on the state*/}
+        <Button
+          onPress={this.onClick}
+          title="Calculate"
+          color="blue"
+          accessibilityLabel="Calculate"
+        />
+		
+        <Text style={styles.footer}>
+          Who is Baldr?{'\n'}
+          In Norse mythology Baldr is the God of Light "insert URL here"
+        </Text>
+		
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e4f9db',
+  },
+  title: {
+    color: '#333333',
+    fontSize: 40,
+	  fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+  },
+  picker: {
+	  color: '#333333',
+	  height: 50,
+    width: 200,
+  },
+  input: {
+    margin: 10,
+	  color: '#333333',
+	  width: 100,
+    fontSize: 25,
+    borderRadius: 4,
+    borderColor: '#ccc',
+	  borderWidth: 1,
+    textAlign: 'center',
+  },
+  calculated: {
+    color: 'green',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    margin: 20,
+  },
+  footer: {
+    textAlign: 'center',
+    margin: 10,
+  }
+});
