3b71175c4b4fcbf4e4efe2e529093d59eb9bf636
Matt Mead <matt.a.mead@gmail.com>
2019-07-26 18:02:30 -0700

    Fixed react-native.config.js
    Removed extra broken settings. All it is doing is providing an asset director for fonts. Was previously in package.json under rnpm.

--- react-native.config.js
+++ react-native.config.js
@@ -1,41 +1,7 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @format
- */
-'use strict';
-
-const ios = require('@react-native-community/cli-platform-ios');
-const android = require('@react-native-community/cli-platform-android');
-
 module.exports = {
-  commands: [...ios.commands, ...android.commands],
-  platforms: {
-    ios: {
-      linkConfig: ios.linkConfig,
-      projectConfig: ios.projectConfig,
-      dependencyConfig: ios.dependencyConfig,
-    },
-    android: {
-      linkConfig: android.linkConfig,
-      projectConfig: android.projectConfig,
-      dependencyConfig: android.dependencyConfig,
-    },
-  },
-  assets: ['./assets/fonts/'],
-  /**
-   * Used when running RNTester (with React Native from source)
-   */
-  reactNativePath: '.',
   project: {
-    ios: {
-      project: './RNTester/RNTesterPods.xcworkspace',
-    },
-    android: {
-      sourceDir: './RNTester',
-    },
+    ios: {},
+    android: {},
   },
+  assets: ['./assets/fonts/']
 };
