e7e5f6c48b4cfbe8a8f08b8179fcb2f841128cf9
Matt Mead <matt.a.mead@gmail.com>
2019-08-27 10:45:55 -0700

    Update to react-native 0.60.5
    Hermes support is fixed. Also updated react-native-gesture-handler to 1.4.1.

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const infoIcon = (<Icon name="info-outline" size={24} />)
 
-let version = 'v0.3.2';
+let version = 'v0.3.3';
 
 class UnfermentedScreen extends React.Component {
   
@@ -377,11 +377,10 @@ class AboutScreen extends React.Component {
             <Text style={styles.medium}>Acknowledgements</Text>
             <Text style={styles.regular}>
               {'\t'}"react": "16.8.6",{'\n'}
-              {'\t'}"react-native": "0.60.4",{'\n'}
-              {'\t'}"react-native-gesture-handler": "^1.3.0",{'\n'}
+              {'\t'}"react-native": "0.60.5",{'\n'}
+              {'\t'}"react-native-gesture-handler": "^1.4.1",{'\n'}
               {'\t'}"react-native-paper": "^2.16.0",{'\n'}
               {'\t'}"react-native-vector-icons": "^6.6.0",{'\n'}
-              {'\t'}"react-native-webview": "^6.3.1",{'\n'}
               {'\t'}"react-navigation": "^3.11.1",{'\n'}
               {'\t'}"react-navigation-material-bottom-tabs": "^1.0.0"
             </Text>
